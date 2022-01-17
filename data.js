5664aab6518bcf9cbbbe274b78fe0711d854077f
Dev <devsendatha@gmail.com>
2022-01-17 20:01:18 +0530

    Initial commit

---
+++ data.js
@@ -0,0 +1,34 @@
+const main = {
+  name: 'Devsen Datha Mukkamala',
+  mail: 'Devsendatham@gmail.com',
+  img:  'profile.jpeg',
+  role: ['Software Engineer'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/Devsendatha'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/Devsendatha/'
+    },
+    {
+      name: 'Twitter',
+      iconName: 'logo-twitter',
+      link: 'https://twitter.com/Devsendatha'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'Devsendatham@gmail.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: 'Projects', link: 'https://github.com/Devsendatha' },
+    { name: 'Blogs', link: 'https://medium.com/@devsendatham' },
+    { name: 'Resume', link: 'https://www.dropbox.com/s/63cubltrlgrr76b/Devsen%20Datha%20Mukkamala%20Resume.pdf?dl=0'},
+  ]
+};
